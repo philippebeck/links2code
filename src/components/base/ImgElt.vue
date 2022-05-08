d35e447a2d92efaeab2b46fcc5037efe5d7450d8
Philippe Beck <philippe@philippebeck.net>
2022-05-08 11:43:21 +0200

    Indent

--- src/components/base/ImgElt.vue
+++ src/components/base/ImgElt.vue
@@ -15,32 +15,32 @@
 </template>
 
 <script>
-  export default {
-    name: "ImgElt",
-    props: {
-      url: {
-        type: String,
-        required: true
-      },
-      info: {
-        type: String,
-        required: true
-      },
-      src: {
-        type: String
-      },
-      media: {
-        type: String,
-        default: "(min-width: 992px)"
-      }
+export default {
+  name: "ImgElt",
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    info: {
+      type: String,
+      required: true
+    },
+    src: {
+      type: String
+    },
+    media: {
+      type: String,
+      default: "(min-width: 992px)"
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  picture {
-    max-width: 100%;
-    height: auto;
-    object-fit: contain;
-  }
+picture {
+  max-width: 100%;
+  height: auto;
+  object-fit: contain;
+}
 </style>
