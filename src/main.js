3b293bd2b9a4e357b90a94dd8f042bf680d3f8aa
Philippe Beck <philippe@philippebeck.net>
2022-05-09 21:58:34 +0200

    Add vuesass to main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import serve from "servidio"
+import vuesass from "vuesass"
 import "./style/style.css"
 
 const app = createApp(App)
 app.config.globalProperties.$serve = serve
-app.use(router).mount("#app")
+app.use(router).use(vuesass).mount("#app")
