e95f612fc1b37bd661a0eb4596c5fa065546d126
Philippe Beck <philippe@philippebeck.net>
2023-01-10 23:25:57 +0400

    Start to update updateUser() with FormData

--- src/components/ListUsers.vue
+++ src/components/ListUsers.vue
@@ -100,27 +100,40 @@ export default {
     },
 
     updateUser(id) {
-      let user = {};
+      let user  = new FormData();
+      let image = "";
+
       for (let i = 0; i < this.users.length; i++ ) {
         if (this.users[i]._id === id) {
-          user = {
-            id: id,
-            name: this.users[i].name,
-            email: this.users[i].email,
-            pass: this.pass
+
+          if (document.getElementById('image').files[0]) {
+            image = document.getElementById('image').files[0];
+          } else {
+            image = this.users[i].image;
           }
+
+          console.log(image);
+
+          user.append("id", id);
+          user.append("name", this.users[i].name);
+          user.append("email", this.users[i].email);
+          user.append("image", image);
+          user.append("pass", this.pass);
         }
       }
-      if (this.$serve.checkString(user.name, "name") === true && 
-        this.$serve.checkString(user.email, "email") === true &&
-        this.$serve.checkString(user.pass, "pass") === true) {
 
-        user.name   = this.$serve.rewriteString(user.name, "name");
-        user.email  = this.$serve.rewriteString(user.email, "email");
+      if (this.$serve.checkString(user.get("name"), "name") === true && 
+        this.$serve.checkString(user.get("email"), "email") === true &&
+        this.$serve.checkString(user.get("pass"), "pass") === true) {
+
+        user.set("name", this.$serve.rewriteString(user.get("name"), "name"));
+        user.set("email", this.$serve.rewriteString(user.get("email"), "email"));
+
+        console.log(user);
 
           this.$serve.putData(`/api/users/${id}`, user)
             .then(() => {
-              alert(user.name + " mis à jour !");
+              alert(user.get("name") + " mis à jour !");
               this.$router.go();
             });
       }
