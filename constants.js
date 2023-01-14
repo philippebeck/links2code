8df5ea409e8aace9038d98aeb622871b1f4a3d6b
Philippe Beck <philippe@philippebeck.net>
2023-01-14 23:44:14 +0400

    Update alert const

--- constants.js
+++ constants.js
@@ -2,17 +2,17 @@
 /* eslint-disable */
 
 export default {
+  ALERT_EMAIL : "Your Email is not a valid address.",
+  ALERT_PASS : "Your Password must have 8 to 50 characters, with uppercase, lowercase, 1 number minimum & no space.",
+  ALERT_URL : "This URL is not valid.",
+
+  API_URL : "http://localhost:3000",
+  CONTENT_TYPE : "multipart/form-data", 
+
   PASS_INT : 1,
   PASS_MAX : 50,
   PASS_MIN : 8,
 
-  ALERT_EMPTY: "Fill in the empty field",
-  ALERT_URL : " is not a valid url",
-  ALERT_EMAIL : " is not a valid email",
-  ALERT_PASS : `Your Password must have 8 characters minimum, 50 characters maximum, with uppercase & lowercase, 1 number minimum & no space.`,
-
-  API_URL : "http://localhost:3000",
-  CONTENT_TYPE : "multipart/form-data", 
   TOKEN : JSON.parse(localStorage.getItem("userToken")),
   USER_ID : JSON.parse(localStorage.getItem("userId"))
 }
