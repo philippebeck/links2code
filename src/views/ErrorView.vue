2f5ef6d00d104fcdf292a1ec915cefc5b838e66d
Philippe Beck <philippe@philippebeck.net>
2023-01-15 11:49:25 +0400

    Create ErrorView

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h1>Page Not Found !</h1>
+
+    <figure>
+      <img 
+      src="img/errors/error-404.webp" 
+      alt="Error 404">
+    </figure>
+
+    <a href="/">
+      <i class="fa-regular fa-hand-point-right"></i>
+      Go back home ?
+      <i class="fa-regular fa-hand-point-left"></i>
+    </a>
+
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'ErrorView'
+  }
+</script>
+
+<style scoped>
+  section {
+    text-align: center;
+  }
+</style>
