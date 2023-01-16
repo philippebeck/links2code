84442a061d09efc45decc191d2d3b462c67861ff
Philippe Beck <philippe@philippebeck.net>
2023-01-16 20:20:56 +0400

    Create forgot part to LoginView template

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,24 +1,25 @@
 <template>
-  <main>
-    <h1 class="violet anima-grow">
+  <main v-if="isLogin">
+    <header class="violet anima-grow">
       <i class="fa-solid fa-sign-in-alt fa-2x"></i>
-      Login
-    </h1>
+      <h1 class="mar-none">Login</h1>
+    </header>
+
     <form class="form">
       <ul>
         <li>
           <FieldElt
             id="email"
             v-model:value="email"
-            info="Indiquer votre identifiant"
+            info="Indicate your Email"
             @keyup.enter="login()"
             type="email"
             required>
             <template #legend>
-              Identifiant
+              Email
             </template>
             <template #label>
-              Vous n'avez rien à faire ici !
+              This email must have been registered before
             </template>
           </FieldElt>
         </li>
@@ -26,15 +27,15 @@
           <FieldElt
             id="pass"
             v-model:value="pass"
-            info="Indiquer votre mot de passe"
+            info="Indicate your Password"
             @keyup.enter="login()"
             type="password"
             required>
             <template #legend>
-              Mot de Passe
+              Password
             </template>
             <template #label>
-              Vous ne passerez pas !
+              You can use the Forgot Password feature if needed
             </template>
           </FieldElt>
         </li>
@@ -50,8 +51,58 @@
         <li>
           <BtnElt
             type="button"
-            content="Connexion"
+            content="Login"
             @click="login()"
+            class="green"/>
+          <BtnElt
+            type="button"
+            content="Forgot Password"
+            @click="toggleFormType()"
+            class="orange"/>
+        </li>
+      </ul>
+    </form>
+  </main>
+
+  <main v-else>
+    <header class="violet anima-grow">
+      <i class="fa-solid fa-key fa-2x"></i>
+      <h1 class="mar-none">Forgot Password</h1>
+    </header>
+
+    <form>
+      <FieldElt
+        id="email"
+        v-model:value="email"
+        info="Indicate your Email"
+        @keyup.enter="forgotPass()"
+        type="email"
+        required>
+        <template #legend>
+          Email
+        </template>
+        <template #label>
+          This email must have been registered before
+        </template>
+      </FieldElt>
+      <ul>
+        <li>
+          <div 
+            id="recaptcha"
+            class="g-recaptcha"
+            data-sitekey="6LdTBtoZAAAAADITfTTXpjsctFXZqKXZc-seM9ZL">
+          </div>
+        </li>
+        <li>
+          <BtnElt
+            type="button"
+            content="Send"
+            @click="forgotPass()"
+            class="orange"/>
+          <BtnElt
+            type="button"
+            content="Login"
+            @click="toggleFormType()"
             class="green"/>
         </li>
       </ul>
