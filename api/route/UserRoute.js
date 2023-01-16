2d887f776b104793f0039e84a2c56cf7bda2181a
Philippe Beck <philippe@philippebeck.net>
2023-01-16 20:18:42 +0400

    Add forgot to UserRoute

--- api/route/UserRoute.js
+++ api/route/UserRoute.js
@@ -6,8 +6,9 @@ const nem       = require("nemjs");
 const UserCtrl  = require("../controller/UserCtrl");
 
 router.get("/", nem.checkAuth, UserCtrl.list);
-router.post("/", nem.checkAuth, UserCtrl.create);
 router.post("/login", UserCtrl.login);
+router.post("/forgot", UserCtrl.forgot);
+router.post("/", nem.checkAuth, UserCtrl.create);
 router.put("/:id", nem.checkAuth, UserCtrl.update);
 router.delete("/:id", nem.checkAuth, UserCtrl.delete);
 router.post("/send", UserCtrl.send);
