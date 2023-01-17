2082bcda4ed915feb46f0d956733fdeb231a3339
Philippe Beck <philippe@philippebeck.net>
2023-01-17 10:13:06 +0400

    Add new const for names

--- constants.js
+++ constants.js
@@ -4,12 +4,16 @@
 export default {
   /* SERVIDIO */
 
+  ALERT_NAME : "The Name must have between 2 & 50 characters.",
   ALERT_EMAIL : "Your Email is not a valid address.",
   ALERT_PASS : "Your Password must have 8 to 50 characters, with uppercase, lowercase, 1 number minimum & no space.",
   ALERT_URL : "This URL is not a valid path.",
 
   API_URL : "http://localhost:3000",
   CONTENT_TYPE : "multipart/form-data", 
+
+  NAME_MIN : 2,
+  NAME_MAX : 50,
 
   PASS_INT : 1,
   PASS_MAX : 50,
