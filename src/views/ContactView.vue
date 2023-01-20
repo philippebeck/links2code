6a0f5275d59307b307c7b5b127d14b1a58799962
Philippe Beck <philippe@philippebeck.net>
2023-01-20 20:41:02 +0400

    Remove main tag from views

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,80 +1,78 @@
 <template>
-  <main>
-    <CardElt>
-      <template #header>
-        <i class="blue fa-solid fa-at fa-2x"></i>
-        <h1 class="blue anima-slideB">
-          Contact
-        </h1>
-      </template>
+  <CardElt>
+    <template #header>
+      <i class="blue fa-solid fa-at fa-2x"></i>
+      <h1 class="blue anima-slideB">
+        Contact
+      </h1>
+    </template>
 
-      <template #body>
-        <form class="anima-grow">
-          <ul>
-            <li>
-              <FieldElt
-                id="email"
-                v-model:value="email"
-                info="An email to answer you ?"
-                @keyup.enter="send()"
-                type="email">
-                <template #legend>
-                  Email
-                </template>
-                <template #label>
-                  Un email fonctionnel svp !
-                </template>
-              </FieldElt>
-            </li>
-            <li>
-              <FieldElt
-                id="subject"
-                v-model:value="subject"
-                info="The subject of your message ?"
-                @keyup.enter="send()">
-                <template #legend>
-                  Subject
-                </template>
-                <template #label>
-                  Soyez bref et précis !
-                </template>
-              </FieldElt>
-            </li>
-            <li>
-              <FieldElt
-                id="text"
-                v-model:value="text"
-                info="The text of your message ?"
-                @keyup.enter="send()"
-                type="area"
-                :max="parseInt('100')">
-                <template #legend>
-                  Text
-                </template>
-                <template #label>
-                  Que puis-je faire pour vous ?
-                </template>
-              </FieldElt>
-            </li>
-            <li>
-              <div 
-                id="recaptcha" 
-                class="g-recaptcha" 
-                data-sitekey="6LdTBtoZAAAAADITfTTXpjsctFXZqKXZc-seM9ZL">
-              </div>
-            </li>
-            <li>
-              <BtnElt
-                type="button"
-                content="Envoyer"
-                @click="send()" 
-                class="green"/>
-            </li>
-          </ul>
-        </form>
-      </template>
-    </CardElt>
-  </main>
+    <template #body>
+      <form class="anima-grow">
+        <ul>
+          <li>
+            <FieldElt
+              id="email"
+              v-model:value="email"
+              info="An email to answer you ?"
+              @keyup.enter="send()"
+              type="email">
+              <template #legend>
+                Email
+              </template>
+              <template #label>
+                Un email fonctionnel svp !
+              </template>
+            </FieldElt>
+          </li>
+          <li>
+            <FieldElt
+              id="subject"
+              v-model:value="subject"
+              info="The subject of your message ?"
+              @keyup.enter="send()">
+              <template #legend>
+                Subject
+              </template>
+              <template #label>
+                Soyez bref et précis !
+              </template>
+            </FieldElt>
+          </li>
+          <li>
+            <FieldElt
+              id="text"
+              v-model:value="text"
+              info="The text of your message ?"
+              @keyup.enter="send()"
+              type="area"
+              :max="parseInt('100')">
+              <template #legend>
+                Text
+              </template>
+              <template #label>
+                Que puis-je faire pour vous ?
+              </template>
+            </FieldElt>
+          </li>
+          <li>
+            <div 
+              id="recaptcha" 
+              class="g-recaptcha" 
+              data-sitekey="6LdTBtoZAAAAADITfTTXpjsctFXZqKXZc-seM9ZL">
+            </div>
+          </li>
+          <li>
+            <BtnElt
+              type="button"
+              content="Envoyer"
+              @click="send()" 
+              class="green"/>
+          </li>
+        </ul>
+      </form>
+    </template>
+  </CardElt>
 </template>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,51 +1,48 @@
 <template>
-  <main id="top">
+  <CardElt id="top">
+    <template #header>
+      <i class="blue fa-solid fa-link fa-2x"></i>
+      <h1 class="blue shatex-blur-sm anima-slideB">
+        Links2Code
+      </h1>
+      <strong class="gray">
+        Links for Coding !
+      </strong>
 
-    <CardElt>
-      <template #header>
-        <i class="blue fa-solid fa-link fa-2x"></i>
-        <h1 class="blue shatex-blur-sm anima-slideB">
-          Links2Code
-        </h1>
-        <strong class="gray">
-          Links for Coding !
-        </strong>
+      <NavElt
+        type="sidebar"
+        :items="cats"
+        class="sidebar">
 
-        <NavElt
-          type="sidebar"
-          :items="cats"
-          class="sidebar">
+        <template #items="slotProps">
+          <i :class="`fa-brands fa-${slotProps.item.toLowerCase()} fa-fw`"></i>
+        </template>
 
-          <template #items="slotProps">
-            <i :class="`fa-brands fa-${slotProps.item.toLowerCase()} fa-fw`"></i>
-          </template>
+        <template #top>
+          <i class="fa-solid fa-chevron-circle-up fa-fw"></i>
+        </template>
+      </NavElt>
+    </template>
 
-          <template #top>
-            <i class="fa-solid fa-chevron-circle-up fa-fw"></i>
-          </template>
-        </NavElt>
-      </template>
+    <template #body>
+      <ListElt :items="itemsByCat(links)">
+        <template #items="slotProps">
+          <i 
+            :class="`fa-brands fa-${slotProps.index.toLowerCase()} fa-6x sky shatex-blur-md anima-grow mar-lg`"
+            :id="slotProps.index">
+          </i>
+        </template>
 
-      <template #body>
-        <ListElt :items="itemsByCat(links)">
-          <template #items="slotProps">
-            <i 
-              :class="`fa-brands fa-${slotProps.index.toLowerCase()} fa-6x sky shatex-blur-md anima-grow mar-lg`"
-              :id="slotProps.index">
-            </i>
-          </template>
-
-          <template #nested="slotProps">
-            <BtnElt
-              :content="slotProps.value.name"
-              :href="`https://${slotProps.value.url}`" 
-              :title="slotProps.value.url"
-              class="blue"/>
-          </template>
-        </ListElt>
-      </template>
-    </CardElt>
-  </main>
+        <template #nested="slotProps">
+          <BtnElt
+            :content="slotProps.value.name"
+            :href="`https://${slotProps.value.url}`" 
+            :title="slotProps.value.url"
+            class="blue"/>
+        </template>
+      </ListElt>
+    </template>
+  </CardElt>
 </template>
 
 <script>
